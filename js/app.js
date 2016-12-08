85701550fa7033afa3952c47e2b8765f297fd7d2
Ezra Raez <ezRAez10@gmail.com>
2016-12-07 22:15:40 -0800

    add comments

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
+// Creates the individual squares in the board
 class Square extends React.Component {
   render() {
+    // The render method is the core feature of React. It will deliver
+    // components of html, with "props" - additional data or behavior to
+    // use in your app.
     return (
       <button className="square">
         {/* TODO */}
@@ -8,8 +12,11 @@ class Square extends React.Component {
   }
 }
 
+// Header with game information above 3x3 grid
 class Board extends React.Component {
   renderSquare(i) {
+    // this component now has a method, renderSquare
+    // its a function to create a square (unnecessary step, for now…)
     return <Square />;
   }
   render() {
@@ -37,6 +44,7 @@ class Board extends React.Component {
   }
 }
 
+// Gathers everything together for additional styling
 class Game extends React.Component {
   render() {
     return (
@@ -55,11 +63,15 @@ class Game extends React.Component {
 
 // ========================================
 
+// Rendering the containing component `Game`, attachign it to the lone
+// #container in the index.html
 ReactDOM.render(
   <Game />,
   document.getElementById('container')
 );
 
+// Checks all available combinations for a winner.
+// Returns the winner or null if neither player has won.
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -69,7 +81,7 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6],
+    [2, 4, 6]
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
