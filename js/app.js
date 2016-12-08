cd8a2d1f724a6b1c30f16f49412ddedb03534da0
Ezra Raez <ezRAez10@gmail.com>
2016-12-07 22:03:12 -0800

    remove this

--- js/app.js
+++ js/app.js
@@ -4,8 +4,8 @@
 // classes
 function Square(props) {
   return (
-    <button className="square" onClick={() => this.props.onClick()}>
-      {this.props.value}
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
     </button>
   );
 }
