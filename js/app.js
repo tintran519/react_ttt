001046aef684953aa1b81efdcf009dc59c7f972c
Ezra Raez <ezRAez10@gmail.com>
2016-12-07 23:23:26 -0800

    remove comma

--- js/app.js
+++ js/app.js
@@ -101,7 +101,7 @@ function calculateWinner(squares) {
     [1, 4, 7],
     [2, 5, 8],
     [0, 4, 8],
-    [2, 4, 6],
+    [2, 4, 6]
   ];
   for (let i = 0; i < lines.length; i++) {
     const [a, b, c] = lines[i];
