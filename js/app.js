fdb2caee21c3638e7a31204487c85ad502eae102
Ezra Raez <ezRAez10@gmail.com>
2016-12-07 22:04:29 -0800

    add line break

--- js/app.js
+++ js/app.js
@@ -25,7 +25,8 @@ class Board extends React.Component {
   }
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+    return <Square value={this.state.squares[i]}
+                   onClick={() => this.handleClick(i)} />;
   }
 
   render() {
